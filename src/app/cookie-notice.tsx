8bb1d7d7d57135e3e61bef97bff8e8ca61010672
Michael Bleigh <bleigh@google.com>
2025-02-03 15:49:49 -0800

    cookie banner ssr fix

--- src/app/cookie-notice.tsx
+++ src/app/cookie-notice.tsx
@@ -7,44 +7,47 @@ export default function CookieNotice({
 }: {
   children: React.ReactNode;
 }) {
+  const [isClient, setIsClient] = useState(false);
   const [cookieAck, setCookieAckInMem] = useState(
     typeof window !== "undefined"
       ? window.localStorage.getItem("__cookieAck") || ""
       : ""
   );
 
-  if (cookieAck !== "true") {
-    return (
-      <>
-        {children}
-        <div className="fixed bottom-0 left-0 right-0 flex justify-center">
-          <div className="text-sm bg-background/90 m-2 p-3 rounded-lg flex max-sm:flex-col items-center border border-primary/20">
-            <div className="mr-2">
-              Genkit by Example uses{" "}
-              <a
-                href="https://policies.google.com/technologies/cookies"
-                className="underlined"
-                target="_blank"
-              >
-                cookies
-              </a>{" "}
-              from Google to deliver and enhance the quality of its services and
-              to analyze traffic.
-            </div>
-            <button
-              className="font-bold text-nowrap px-2 py-1 border border-primary/20 rounded"
-              onClick={() => {
-                window.localStorage.setItem("__cookieAck", "true");
-                setCookieAckInMem("true");
-              }}
-            >
-              I understand.
-            </button>
-          </div>
-        </div>
-      </>
-    );
-  }
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-  return children;
+  if (cookieAck === "true" || !isClient) return children;
+
+  return (
+    <>
+      {children}
+      <div className="fixed bottom-0 left-0 right-0 flex justify-center">
+        <div className="text-sm bg-background/90 m-2 p-3 rounded-lg flex max-sm:flex-col items-center border border-primary/20">
+          <div className="mr-2">
+            Genkit by Example uses{" "}
+            <a
+              href="https://policies.google.com/technologies/cookies"
+              className="underline"
+              target="_blank"
+            >
+              cookies
+            </a>{" "}
+            from Google to deliver and enhance the quality of its services and
+            to analyze traffic.
+          </div>
+          <button
+            className="font-bold text-nowrap px-2 py-1 border border-primary/20 rounded"
+            onClick={() => {
+              window.localStorage.setItem("__cookieAck", "true");
+              setCookieAckInMem("true");
+            }}
+          >
+            I understand.
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
