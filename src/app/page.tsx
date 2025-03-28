e13087d5290ab259b680a410451dd7057f813618
Michael Bleigh <bleigh@google.com>
2025-03-28 09:38:11 -0700

    more tweaks to brand update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,51 +21,46 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Add GenAI to your web/mobile app",
-  description:
-    "Pre-built examples of using Genkit to build a variety of AI-powered features for Next.js applications.",
-  openGraph: {
-    images: [
-      `${
-        process.env.SITE_ORIGIN || "http://localhost:3000"
-      }/api/og?title=Practical%20GenAI%20 Demos`,
-    ],
-  },
+	title: "Add GenAI to your web/mobile app",
+	description:
+		"Pre-built examples of using Genkit to build a variety of AI-powered features for Next.js applications.",
+	openGraph: {
+		images: [
+			`${
+				process.env.SITE_ORIGIN || "http://localhost:3000"
+			}/api/og?title=Practical%20GenAI%20 Demos`,
+		],
+	},
 };
 
 export default function Home() {
-  return (
-    <div className="my-4 max-w-screen-md mx-auto p-4">
-      <h1 className="text-4xl text-center sm:mt-6 mb-4 font-semibold flex justify-center">
-        <Image
-          src="/banner.png"
-          alt="Genkit"
-          width={600}
-          height={200}
-        />
-      </h1>
-      <p>
-        <b>Genkit by Example</b> is a collection of pre-built examples of using
-        Genkit to build a variety of AI-powered features for Next.js
-        applications.
-      </p>
-      <p className="mt-4">
-        Each example highlights a specific feature or pattern of Genkit and
-        includes source code references and a working demo. Choose an example
-        from the menu on the left or click a link below to get started:
-      </p>
-      <div className="p-4 mt-6 grid grid-cols-2 max-sm:grid-cols-1">
-        {demos.map((d) => (
-          <Link href={`/${d.id}`} key={d.id}>
-            <Card className="p-2 sm:mx-2 my-4 hover:bg-orange-500/10">
-              <h4 className="text-nowrap block p-2 text-orange-200 rounded-lg">
-                {d.name}
-              </h4>
-              <p className="text-sm p-2">{d.description}</p>
-            </Card>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div className="my-4 max-w-screen-md mx-auto p-4">
+			<h1 className="text-4xl text-center sm:mt-6 mb-4 font-semibold flex justify-center">
+				<Image src="/banner.png" alt="Genkit" width={600} height={200} />
+			</h1>
+			<p>
+				<b>Genkit by Example</b> is a collection of pre-built examples of using
+				Genkit to build a variety of AI-powered features for Next.js
+				applications.
+			</p>
+			<p className="mt-4">
+				Each example highlights a specific feature or pattern of Genkit and
+				includes source code references and a working demo. Choose an example
+				from the menu on the left or click a link below to get started:
+			</p>
+			<div className="p-4 mt-6 grid grid-cols-2 max-sm:grid-cols-1">
+				{demos.map((d) => (
+					<Link href={`/${d.id}`} key={d.id}>
+						<Card className="p-2 sm:mx-2 my-4 hover:bg-purple-500/10">
+							<h4 className="text-nowrap block p-2 text-purple-200 rounded-lg">
+								{d.name}
+							</h4>
+							<p className="text-sm p-2">{d.description}</p>
+						</Card>
+					</Link>
+				))}
+			</div>
+		</div>
+	);
 }
--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -20,17 +20,17 @@ import * as React from "react";
 import { usePathname } from "next/navigation";
 
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarMenuSub,
-  SidebarMenuSubButton,
-  SidebarMenuSubItem,
-  SidebarSeparator,
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarMenuSub,
+	SidebarMenuSubButton,
+	SidebarMenuSubItem,
+	SidebarSeparator,
 } from "@/components/ui/sidebar";
 import Image from "next/image";
 import Link from "next/link";
@@ -38,91 +38,91 @@ import { demos } from "@/data";
 import { BookText, Github, Lightbulb, Newspaper } from "lucide-react";
 
 const genkitLinks = [
-  {
-    url: "https://github.com/genkit-ai/genkit-by-example/issues/new?template=suggest-idea-for-example.md",
-    label: "Suggest an Idea",
-    icon: Lightbulb,
-  },
-  {
-    url: "https://firebase.google.com/docs/genkit",
-    label: "Documentation",
-    icon: BookText,
-  },
-  {
-    url: "https://github.com/firebase/genkit",
-    label: "GitHub",
-    icon: Github,
-  },
-  {
-    url: "https://firebase.blog/category/genkit",
-    label: "Blog",
-    icon: Newspaper,
-  },
+	{
+		url: "https://github.com/genkit-ai/genkit-by-example/issues/new?template=suggest-idea-for-example.md",
+		label: "Suggest an Idea",
+		icon: Lightbulb,
+	},
+	{
+		url: "https://firebase.google.com/docs/genkit",
+		label: "Documentation",
+		icon: BookText,
+	},
+	{
+		url: "https://github.com/firebase/genkit",
+		label: "GitHub",
+		icon: Github,
+	},
+	{
+		url: "https://firebase.blog/category/genkit",
+		label: "Blog",
+		icon: Newspaper,
+	},
 ];
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const pathname = usePathname();
-  return (
-    <Sidebar variant="floating" {...props}>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild>
-              <Link href="/" className="flex justify-center">
-                <Image
-                  src="/genkit_logo.png"
-                  alt="Genkit logo"
-                  width={36}
-                  height={36}
-                />
-                <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-bold text-xl">
-                    Genkit{" "}
-                    <span className="text-sm text-sidebar-primary-foreground/60">
-                      by Example
-                    </span>
-                  </span>
-                </div>
-              </Link>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarMenu className="gap-2">
-            {demos.map((item) => (
-              <SidebarMenuItem key={item.id}>
-                <SidebarMenuButton
-                  asChild
-                  isActive={pathname === `/${item.id}`}
-                >
-                  <Link href={`/${item.id}`}>{item.name}</Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            ))}
-          </SidebarMenu>
-        </SidebarGroup>
-        <SidebarSeparator />
-        <SidebarGroup>
-          <SidebarMenu className="gap-2">
-            {genkitLinks.map((link, i) => (
-              <SidebarMenuItem key={i}>
-                <SidebarMenuButton>
-                  <a
-                    href={link.url}
-                    className="flex items-center"
-                    target="_blank"
-                  >
-                    {<link.icon className="w-4 h-4 mr-1" />}
-                    <span>{link.label}</span>
-                  </a>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            ))}
-          </SidebarMenu>
-        </SidebarGroup>
-      </SidebarContent>
-    </Sidebar>
-  );
+	const pathname = usePathname();
+	return (
+		<Sidebar variant="floating" {...props}>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton size="lg" asChild>
+							<Link href="/" className="flex justify-center">
+								<Image
+									src="/genkit_logo.png"
+									alt="Genkit logo"
+									width={28}
+									height={28}
+								/>
+								<div className="flex flex-col leading-none">
+									<span className="font-bold text-xl">
+										Genkit{" "}
+										<span className="text-sm text-sidebar-primary-foreground/60">
+											by Example
+										</span>
+									</span>
+								</div>
+							</Link>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarMenu className="gap-2">
+						{demos.map((item) => (
+							<SidebarMenuItem key={item.id}>
+								<SidebarMenuButton
+									asChild
+									isActive={pathname === `/${item.id}`}
+								>
+									<Link href={`/${item.id}`}>{item.name}</Link>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+						))}
+					</SidebarMenu>
+				</SidebarGroup>
+				<SidebarSeparator />
+				<SidebarGroup>
+					<SidebarMenu className="gap-2">
+						{genkitLinks.map((link, i) => (
+							<SidebarMenuItem key={i}>
+								<SidebarMenuButton>
+									<a
+										href={link.url}
+										className="flex items-center"
+										target="_blank"
+									>
+										{<link.icon className="w-4 h-4 mr-1" />}
+										<span>{link.label}</span>
+									</a>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+						))}
+					</SidebarMenu>
+				</SidebarGroup>
+			</SidebarContent>
+		</Sidebar>
+	);
 }
